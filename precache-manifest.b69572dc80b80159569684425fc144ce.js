257644ac55ff01b50b03ae65b28a2c1b54c4a61a
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-05-31 23:39:27 -0400

    Updates

---
+++ precache-manifest.b69572dc80b80159569684425fc144ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c897416009140ffe4f6d",
+    "url": "/project-management-mini-app/static/css/main.b6c03e2b.chunk.css"
+  },
+  {
+    "revision": "c897416009140ffe4f6d",
+    "url": "/project-management-mini-app/static/js/main.c8974160.chunk.js"
+  },
+  {
+    "revision": "7d2fb3161dc896e47ffe",
+    "url": "/project-management-mini-app/static/css/1.a8cf723b.chunk.css"
+  },
+  {
+    "revision": "7d2fb3161dc896e47ffe",
+    "url": "/project-management-mini-app/static/js/1.7d2fb316.chunk.js"
+  },
+  {
+    "revision": "a624f5a506a884bb0d20",
+    "url": "/project-management-mini-app/static/js/runtime~main.a624f5a5.js"
+  },
+  {
+    "revision": "24638cbe361c7d3cb02985e53cfa4753",
+    "url": "/project-management-mini-app/static/media/desk.24638cbe.jpg"
+  },
+  {
+    "revision": "b0019ac604202e38b5ea69025a583f2d",
+    "url": "/project-management-mini-app/index.html"
+  }
+];
